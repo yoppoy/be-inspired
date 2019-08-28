fe664996cf55f683a01a1ee83e549167addc4076
yan <yan.poinssot@gmail.com>
2019-08-28 18:58:25 +0800

    add (server coors)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const graphqlConfig = require('./graphql/index.js');
+const cors = require('cors');
 let app = express();
 
+app.use(cors()) // enable `cors` to set HTTP response header: Access-Control-Allow-Origin: *
 app.use('/graphql', graphqlConfig);
 app.listen(4000, () => console.log('Server running on localhost:4000/graphql'));
