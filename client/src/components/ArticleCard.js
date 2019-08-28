c296672d9a3ab375c2847c0d322b0dd1c80f42e9
yan <yan.poinssot@gmail.com>
2019-08-28 19:09:08 +0800

    fix (card display)

--- client/src/components/ArticleCard.js
+++ client/src/components/ArticleCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -13,11 +14,11 @@ const useStyles = makeStyles({
         maxWidth: 345,
     },
     media: {
-        height: 140,
+        height: "inherit"
     },
 });
 
-export default function ArticleCard({title, description, author}) {
+export default function ArticleCard({title, description, url}) {
     const classes = useStyles();
 
     return (
--- client/src/containers/Board.js
+++ client/src/containers/Board.js
@@ -3,14 +3,23 @@ import {useQuery} from '@apollo/react-hooks';
 import {gql} from 'apollo-boost';
 import Grid from '@material-ui/core/Grid';
 import ArticleCard from "../components/ArticleCard";
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    cardContainer: {
+        padding: 10,
+    }
+});
 
 export default function Board() {
+    const classes = useStyles();
     const {loading, error, data} = useQuery(gql`
         {
             articles {
                 title
                 author
                 description
+                url
             }
         }
     `);
@@ -25,8 +34,10 @@ export default function Board() {
             justify="center"
             alignItems="center"
         >
-            {data.articles.map(({title, author, description}) => (
-                <ArticleCard key={title} title={title} author={author} description={description}/>
+            {data.articles.map(({title, url, description, author}) => (
+                <div className={classes.cardContainer}>
+                    <ArticleCard key={title} title={title} author={author} description={description} url={url}/>
+                </div>
             ))}
         </Grid>
 
